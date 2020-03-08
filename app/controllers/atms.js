22dad7eab913c7c35359a5e4f48b2aa26f622bdb
Adam <adam@Valentinas-iMac.local>
2020-03-08 22:20:07 +0000

    Ent_Web_Dev_Assignment-1_V2.5.0

---
+++ app/controllers/atms.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const Addition = require('../models/addition');
+const User = require('../models/user');
+//const Candidate = require('../models/atm');
+
+const Atms = {
+  home: {
+    handler: async function(request, h) {
+      return h.view('home', { title: 'Make a Addition' });
+    }
+  },
+  list: {
+    handler: async function(request, h) {
+      try {
+        const additions = await Addition.find().populate('donor').lean();
+        return h.view('list', {
+          title: 'Atms to Date',
+          additions: additions
+        });
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  },
+  addAtm: {
+    handler: async function(request, h) {
+      try {
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id);
+        const data = request.payload;
+
+/*        const rawCandidate = request.payload.candidate.split(',');
+        const candidate = await Candidate.findOne({
+          atmName: rawCandidate[0],
+          atmDescription: rawCandidate[1],
+          atmCategory: rawCandidate[2]
+        });
+*/
+
+        const newAddition = new Addition({
+          name: data.name,
+          category: data.category,
+          description: data.description,
+/*        candidate: candidate._id */
+        });
+        await newAddition.save();
+        return h.redirect('/list');
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
+    }
+  }
+};
+
+module.exports = Atms;
---
+++ routes.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const Accounts = require('./app/controllers/accounts');
+const Additions = require('./app/controllers/atms');
+const Gallery = require('./app/controllers/gallery');
+
+module.exports = [
+  { method: 'GET', path: '/', config: Accounts.index },
+  { method: 'GET', path: '/signup', config: Accounts.showSignup },
+  { method: 'GET', path: '/login', config: Accounts.showLogin },
+  { method: 'GET', path: '/logout', config: Accounts.logout },
+  { method: 'POST', path: '/signup', config: Accounts.signup },
+  { method: 'POST', path: '/login', config: Accounts.login },
+  { method: 'GET', path: '/settings', config: Accounts.showSettings },
+  { method: 'POST', path: '/settings', config: Accounts.updateSettings },
+
+  { method: 'GET', path: '/gallery', config: Gallery.index },
+  { method: 'POST', path: '/uploadfile', config: Gallery.uploadFile },
+  { method: 'GET', path: '/deleteimage/{id}', config: Gallery.deleteImage },
+
+  { method: 'GET', path: '/home', config: Additions.home },
+  { method: 'GET', path: '/list', config: Additions.list },
+  { method: 'POST', path: '/addAtm', config: Additions.addAtm },
+  {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: './public'
+      }
+    },
+    options: { auth: false }
+  }
+];
